57c189630a876fb2739bf7b69efef97e616a4670
Nermin Skenderovic <nermin@skenderovic.se>
2019-11-04 16:57:14 +0100

    Second commit

--- scrape.js
+++ scrape.js
@@ -1,45 +1,82 @@
 const request = require('request')
 const cheerio = require('cheerio')
 
-const CARDNUMBER = 3819297030875221;
+const siteURL = 'https://kortladdning3.chalmerskonferens.se'
+const CARDNUMBER = 3819297030875221
 
-let formData = {
-  __VIEWSTATE: '',
-  __VIEWSTATEGENERATOR: '',
-  __EVENTVALIDATION: '',
-  hiddenIsMobile: 'desktop',
-  txtCardNumber: CARDNUMBER,
-  SavedCardNumber: '',
-  btnNext: 'N%C3%A4sta'
-}
+let cookieJar = request.jar();
+let cookieString;
 
 let options = {
-  url: 'https://kortladdning3.chalmerskonferens.se',
-  form: formData
+  url: siteURL,
+  jar: cookieJar,
+  headers: {
+    'Cookie': 'AspxAutoDetectCookieSupport=1; ASP.NET_SessionId=zdzcs15tf2yvvbysjdwjix15; cookieconsent_dismissed=yes; userInfo=A867718FF0054D7E15A4ACF5F1BC8709F3018376A78F1DF977007524C2AA6A3C0225B79F1C0A4688816BB4BA9D922349B87B099E20DF776A609E09A1353124D9347679804859A8EAD9AFF6D4345897E766DEF9AC3E4003395CA0DF035DEA210D94831FD39E19F552CE090F3EA0BC670A1D53A8E0709406C0DA2D4613868876CDD34CBBB6237B23FC223BD8B44EE96989AD04C2B9CAF3CEC497C7E7531214EEBBFDF18DA18E061C0F093A438DB0C500BB9E83C10C84BC19BD02A4FA64977F5D1B9DC0BF8359AB44C0D9B3D2F67FFD165240AAA60210414DBADEAC5CF5CA3EB601B5ABBAC899F12BA7C4528E287F18F259; ASP.NET_SessionId=edltaeeo3jvuo4zv4dtvaej0',
+    'User-Agent': '', // Any User-Agent header must be sent,
+    'Referer': 'https://kortladdning3.chalmerskonferens.se/',
+    'Connection': 'keep-alive',
+    'Accept-Encoding': 'br', // mby not necessary
+    'Cache-Control': 'no-cache',
+    'Accept': '*/*'
+  }
 }
 
-request('https://kortladdning3.chalmerskonferens.se', (err, res, body) => {
-  if (err || res.statusCode !== 200) return
-  const $ = cheerio.load(body)
-  console.log($.html())
+let test = {
+  url: 'https://kortladdning3.chalmerskonferens.se',
+  // method: 'get',
+  jar: cookieJar,
+}
 
-  const viewstate = $('#__VIEWSTATE').attr('value')
-  const viewstategenerator = $('#__VIEWSTATEGENERATOR').attr('value')
-  const eventvalidation = $('#__EVENTVALIDATION').attr('value')
+// supposed to be self-invoking function to do everything
+function main() {
 
-  formData.__VIEWSTATE = viewstate
-  formData.__VIEWSTATEGENERATOR = viewstategenerator
-  formData.__EVENTVALIDATION = eventvalidation
-  console.log(formData)
+}
 
-  request(options, callback)
+request.get({ url: siteURL, jar: cookieJar }, (err, res, body) => {
+  getSessionCookie(err, res, body)
+  request.post(options, (err, res, body) => {
+    // printResult(err, res, body)
+  })
 })
 
-function callback(err, res, body) {
+function getSessionCookie(err, res, body) {
+  // if (err || res.statusCode !== 200) { console.log(err, res.statusCode); return }
+  cookieString = cookieJar.getCookieString(siteURL)
+  options.headers.Cookie = cookieString
+  console.log(options.headers.Cookie);
+}
+
+function printResult(err, res, body) {
+  // if (err || res.statusCode !== 200) { console.log(err, res.statusCode); return }
+  const $ = cheerio.load(body)
+  console.log(res.headers)
+
+  const html = $.html()
+  // console.log(html)
+  // console.log($('document'))
+
+
+  const cookies = cookieJar.getCookies(options.url)
+  console.log(cookies)
+
+  console.log($('#txtPTMCardValue').html())
+}
+
+return
+request(options, (err, res, body) => {
   if (err || res.statusCode !== 200) return
   const $ = cheerio.load(body)
-  console.log($.html())
+  console.log(res.headers)
 
-  const captcha = $('.LBD_CaptchaDiv')
-  console.log(captcha)
-}
+  const html = $.html()
+  // console.log(html)
+  // console.log($('document'))
+
+
+  const cookie_string = cookieJar.getCookies(options.url)
+  const cookies = cookieJar.getCookies(options.url)
+  console.log(cookie_string, cookies)
+  console.log(cookieJar)
+
+  console.log($('#txtPTMCardValue').html())
+})
